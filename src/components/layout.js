a4e6ec433b7f818caaad89737a367b63910f1013
Dan Kempton <kemptondan@gmail.com>
2021-06-24 13:43:31 -0400

    installing normalize.css package

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,9 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import "normalize.css"
 
-import Header from "./header"
+import Header from "./Header/header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
