0062d010f7da4e5253ae93c9e01b46350f088d54
Dan Kempton <kemptondan@gmail.com>
2021-06-28 15:47:28 -0400

    adding header Links

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,25 +6,19 @@ import classes from "./header.scss"
 
 const Header = ({ siteTitle }) => (
   <header className="header">
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   </header>
 )
 
