2da47a2f60c39443556b07928d081c28580a64d1
Dan Kempton <kemptondan@gmail.com>
2021-07-27 22:19:57 -0400

    adding a TextBlock component for wrapping <p> text

---
+++ src/components/TextBlock/TextBlock.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./TextBlock.scss"
+
+function TextBlock({ children }) {
+  return <div className="text-block">{children}</div>
+}
+
+export default TextBlock
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Seo from "../components/seo"
 
 import MainHeading from "../components/MainHeading/MainHeading"
 import SubHeading from "../components/SubHeading/SubHeading"
+import TextBlock from "../components/TextBlock/TextBlock"
 
 const IndexPage = () => (
   <Layout>
@@ -23,6 +24,26 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     /> */}
+    <TextBlock>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </TextBlock>
   </Layout>
 )
 
