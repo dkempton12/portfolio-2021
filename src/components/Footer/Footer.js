bad5045db8686d81fef7e8ae95e0736463112caa
Dan Kempton <kemptondan@gmail.com>
2021-08-25 20:40:08 -0400

    adding footer icon images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from "react"
+
 import "./Footer.scss"
+
+import gitLogo from "../../assets/images/svg/github-logo.svg"
+import linkedInLogo from "../../assets/images/svg/linkedin.svg"
 
 function Footer() {
   return (
     <footer className="footer">
       <p>Lorem ipsum lorem ipsum lorem ipsum</p>
+      <div className="footer__images">
+        <span className="footer-social">
+          <img src={gitLogo} alt="GitHub Account" />
+        </span>
+        <span className="footer-social">
+          <img src={linkedInLogo} alt="LinkedIn Account" />
+        </span>
+      </div>
       <span>
         © {new Date().getFullYear()} Danny Kempton
         {` `}
